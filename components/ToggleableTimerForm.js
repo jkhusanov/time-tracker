c12942eb937a6834558c9936e30e9e4ab3484ff9
Jahon <worldj95@gmail.com>
2019-06-30 12:35:23 -0700

    Implement prop functions

--- components/ToggleableTimerForm.js
+++ components/ToggleableTimerForm.js
@@ -4,16 +4,29 @@ import { StyleSheet, View } from 'react-native';
 import TimerButton from './TimerButton';
 import TimerForm from './TimerForm';
 
-export default function ToggleableTimerForm() {
+export default function ToggleableTimerForm({ onFormSubmit }) {
   const [isOpen, setIsOpen] = useState(false);
 
-  handleFormOpen = () => {
+  const handleFormOpen = () => {
     setIsOpen(true);
+  };
+
+  const handleFormClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleFormSubmit = timer => {
+    onFormSubmit(timer);
+    setIsOpen(false);
   };
 
   return (
     <View style={[styles.container, !isOpen && styles.buttonPadding]}>
-      {isOpen ? <TimerForm /> : <TimerButton title="+" color="black" onPress={handleFormOpen} />}
+      {isOpen ? (
+        <TimerForm onFormSubmit={handleFormSubmit} onFormClose={handleFormClose} />
+      ) : (
+        <TimerButton title="+" color="black" onPress={handleFormOpen} />
+      )}
     </View>
   );
 }
