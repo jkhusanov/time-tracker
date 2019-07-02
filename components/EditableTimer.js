de05a1d7885694cc2f03472cb28ba25afa966114
Jahon <worldj95@gmail.com>
2019-07-02 13:31:35 -0700

    Update EditableTimer to handle form open and close and passing props  to children and parent

--- components/EditableTimer.js
+++ components/EditableTimer.js
@@ -1,14 +1,62 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 import TimerForm from './TimerForm';
 import Timer from './Timer';
 
-export default function EditableTimer({ id, title, project, elapsed, isRunning }) {
+export default function EditableTimer({ id, title, project, elapsed, isRunning, onFormSubmit }) {
   const [editFormOpen, setEditFormOpen] = useState(false);
 
+  const handleEditPress = () => {
+    openForm();
+  };
+
+  const handleFormClose = () => {
+    closeForm();
+  };
+
+  const handleSubmit = timer => {
+    onFormSubmit(timer);
+    closeForm();
+  };
+
+  const closeForm = () => {
+    setEditFormOpen(false);
+  };
+
+  const openForm = () => {
+    setEditFormOpen(true);
+  };
+
   if (editFormOpen) {
-    return <TimerForm id={id} title={title} project={project} />;
+    return (
+      <TimerForm
+        id={id}
+        title={title}
+        project={project}
+        onFormSubmit={handleSubmit}
+        onFormClose={handleFormClose}
+      />
+    );
   }
 
-  return <Timer id={id} title={title} project={project} elapsed={elapsed} isRunning={isRunning} />;
+  return (
+    <Timer
+      id={id}
+      title={title}
+      project={project}
+      elapsed={elapsed}
+      isRunning={isRunning}
+      onEditPress={handleEditPress}
+    />
+  );
 }
+
+EditableTimer.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  project: PropTypes.string.isRequired,
+  elapsed: PropTypes.string.isRequired,
+  isRunning: PropTypes.bool.isRequired,
+  onFormSubmit: PropTypes.func.isRequired,
+};
