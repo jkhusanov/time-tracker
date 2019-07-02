c550d11e9915d2e5ac90d78314e055fc4bb894af
Jahon <worldj95@gmail.com>
2019-07-02 13:19:44 -0700

    Add editability to Timer

--- components/Timer.js
+++ components/Timer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import PropTypes from 'prop-types';
 
 import { millisecondsToHuman } from '../utils/TimerUtils';
 import TimerButton from './TimerButton';
 
-export default function Timer({ title, project, elapsed }) {
+export default function Timer({ title, project, elapsed, onEditPress }) {
   const elapsedString = millisecondsToHuman(elapsed);
 
   return (
@@ -13,13 +14,21 @@ export default function Timer({ title, project, elapsed }) {
       <Text>{project}</Text>
       <Text style={styles.elapsedTime}>{elapsedString}</Text>
       <View style={styles.buttonGroup}>
-        <TimerButton color="blue" small title="Edit" />
+        <TimerButton color="blue" small title="Edit" onPress={onEditPress} />
         <TimerButton color="blue" small title="Remove" />
       </View>
       <TimerButton color="#21BA45" title="Start" />
     </View>
   );
 }
+
+Timer.propTypes = {
+  // id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  project: PropTypes.string.isRequired,
+  elapsed: PropTypes.string.isRequired,
+  onEditPress: PropTypes.func.isRequired,
+};
 
 const styles = StyleSheet.create({
   timerContainer: {
